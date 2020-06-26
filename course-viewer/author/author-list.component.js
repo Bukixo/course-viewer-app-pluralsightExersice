6db1390e8a350033c91295eaff5846ae936a9391
Buki Thompson <bukithompson@Bukis-MacBook-Air.local>
2020-06-26 09:57:23 +0100

    inital commit

--- course-viewer/author/author-list.component.js
+++ course-viewer/author/author-list.component.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module('courseViewer').component('', {
+    angular.module('courseViewer').component('authorList', {
         bindings: {
         },
         controllerAs: 'vm',
